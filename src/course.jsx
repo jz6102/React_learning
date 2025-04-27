4c0358bac01a481b1d00d56f9bde8381ac2fa93c
JAIKANNA.B <117563781+jz6102@users.noreply.github.com>
2025-04-27 17:22:46 +0530

    learned upto methods as props from code.io youtube

--- src/course.jsx
+++ src/course.jsx
@@ -1,11 +1,15 @@
 
 
 function Course(props) {
+  function Buycourse(discount) {
+    alert( props.name+" purchased on "+discount+"% discount successfully!");
+  }
   return (
     <div className='card'>
         <img src={props.img} alt="react img" />
         <h3>{props.name}</h3>
         <p>{props.price}</p>
+        <button className="btn" onClick={() => Buycourse(20)}>Buy Now</button>
     </div>
   );
 }
--- src/courseList.jsx
+++ src/courseList.jsx
@@ -12,71 +12,74 @@ import lit from './assets/lit.svg';
 
 function CourseList() {
     
-    const Courselist = [
+    const courselist = [
         {
             id: 1,
             name: "React",
-            price: "$100",
+            price: "$"+150,
             img: react
         },
         {
             id: 2,
             name: "Angular",
-            price: "$100",
+            price: "$"+150,
             img: ang
         },
         {
             id: 3,
             name: "Next",
-            price: "$100",
+            price: "$"+150,
             img: nxt
         },
         {
             id: 4,
             name: "Vue",
-            price: "$100",
+            price: "$"+100,
             img: vue
         },
         {
             id: 5,
             name: "Svelte",
-            price: "$100",
+            price: "$"+100,
             img: svelte
         },
         {
             id: 6,
             name: "Ember",
-            price: "$100",
+            price: "$"+100,
             img: ember
         },
         {
             id: 7,
             name: "Backbone",
-            price: "$100",
+            price: "$"+100,
             img: backbone
         },
         {
             id: 8,
             name: "Meteor",
-            price: "$100",
+            price: "$"+110,
             img: meteor
         },
         {
             id: 9,
             name: "Preact",
-            price: "$100",
+            price: "$"+120,
             img: preact
         },
         {
             id: 10,
             name: "Lit",
-            price: "$100",
+            price: "$"+130,
             img: lit
         },
     ];
+
+    courselist.sort((a, b) => b.price - a.price);
+
     return (
         <div className='course-list'>
-            {Courselist.map((course) => {
+            {courselist.map((course) => {
                 return (
                     <Course key={course.id} name={course.name} price={course.price} img={course.img} />
                 );
